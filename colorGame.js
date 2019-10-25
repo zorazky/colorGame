af9505a4908bb10b61d8829977434a8a59410eed
zorazky <oswaldo.vazquez@gmail.com>
2019-10-25 12:07:02 -0500

    generating random colors

--- colorGame.js
+++ colorGame.js
@@ -1,15 +1,10 @@
-const colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"]
+const colors = generateRandomColors(6);
 
 const squares = document.querySelectorAll(".square");
 const pickedColor = pickColor();
 const colorDisplay = document.getElementById("colorDisplay");
 const messageDisplay = document.querySelector("#message");
+const h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -27,6 +22,7 @@ squares.forEach((square, i) => {
             console.log("correct")
             messageDisplay.textContent = "Correct";
             changeColors(clickedColor)
+            h1.style.backgroundColor = clickedColor;
         } else {
             console.log("wrong")
             this.style.background = "#232323";
@@ -44,4 +40,22 @@ function changeColors(color) {
 function pickColor() {
     const random = Math.floor(Math.random() * colors.length);
     return colors[random];
+}
+
+function generateRandomColors(num) {
+    let arr = [];
+
+    for (let i = 0; i < num; i++) {
+        arr.push(randomColor())
+    }
+
+    return arr;
+}
+
+function randomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`;
 }
