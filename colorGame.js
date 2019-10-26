3250d3460576d2ce1156b4661551d770c52f53e8
zorazky <oswaldo.vazquez@gmail.com>
2019-10-26 08:34:15 -0500

    easy and hard functions working

--- colorGame.js
+++ colorGame.js
@@ -1,10 +1,65 @@
-const colors = generateRandomColors(6);
+let numberOfSquares = 6;
+let colors = generateRandomColors(numberOfSquares);
 
 const squares = document.querySelectorAll(".square");
-const pickedColor = pickColor();
-const colorDisplay = document.getElementById("colorDisplay");
+let pickedColor = pickColor();
+let colorDisplay = document.getElementById("colorDisplay");
 const messageDisplay = document.querySelector("#message");
 const h1 = document.querySelector("h1");
+const resetButton = document.querySelector("#reset");
+const easyBtn = document.querySelector("#easyBtn");
+const hardBtn = document.querySelector("#hardBtn");
+
+
+easyBtn.addEventListener("click", function() {
+    hardBtn.classList.remove("selected")
+    easyBtn.classList.add("selected")
+    numberOfSquares = 3;
+
+    colors = generateRandomColors(numberOfSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+
+    squares.forEach((square, i) => {
+        if (colors[i]) {
+            square.style.backgroundColor = colors[i];
+        } else {
+            console.log(squares[i])
+            square.style.display = "none";
+        }
+        //square.style.backgroundColor = colors[i];
+    })
+    console.log(squares)
+    console.log(colors)
+});
+
+hardBtn.addEventListener("click", function() {
+    hardBtn.classList.add("selected")
+    easyBtn.classList.remove("selected")
+    numberOfSquares = 6;
+
+    colors = generateRandomColors(numberOfSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+
+    squares.forEach((square, i) => {
+        
+        square.style.backgroundColor = colors[i];   
+        square.style.display = "block";
+       
+    })
+});
+
+resetButton.addEventListener("click", function() {
+    colors = generateRandomColors(numberOfSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    messageDisplay.textContent = "";
+    squares.forEach((square, i) => {
+        square.style.backgroundColor = colors[i];
+    })
+    h1.style.backgroundColor = "#232323";
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -21,6 +76,7 @@ squares.forEach((square, i) => {
         if (clickedColor === pickedColor) {
             console.log("correct")
             messageDisplay.textContent = "Correct";
+            resetButton.textContent = "Play Again?";
             changeColors(clickedColor)
             h1.style.backgroundColor = clickedColor;
         } else {
